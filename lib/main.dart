8873932434c65bb936b9cdda98305e0eabd015e3
Gualter Mesquita <gualter.mesquita@leucotron.com.br>
2023-04-09 23:59:28 -0300

    Criação da tela de boas vindas, definição de estilo padrão, inserção de assets de fonte e imagens estáticas.

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,165 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ReservApp',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        colorScheme: const ColorScheme(
+            brightness: Brightness.light,
+            primary: Color(0xFFF58008),
+            onPrimary: Colors.white,
+            secondary: Color(0xFFF58008),
+            onSecondary: Colors.white,
+            tertiary: Color(0xFFD9D9D9),
+            onTertiary: Colors.white,
+            error: Colors.red,
+            onError: Colors.black,
+            background: Color(0xFFFDFDFD),
+            onBackground: Colors.black,
+            surface: Colors.white,
+            onSurface: Colors.grey
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(foregroundColor: const Color(0xFFF58008)),
+        ),
+        //primarySwatch: Colors.orange,
+
+        fontFamily: 'Quicksand'
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TelaBemVindo(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class TelaBemVindo extends StatelessWidget{
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        padding: const EdgeInsets.only(left: 5.0, right: 5.0),
+        children: [
+          Image.asset(
+            'lib/assets/food.png',
+            width: 360,
+            height: 240,
+            fit: BoxFit.cover,
+          ),
+          welcomeText,
+          welcomeSubtext,
+          const RegisterButton(),
+          const LoginButton(),
+        ],
+      ),
+    );
+  }
+  
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+Widget welcomeText = const Padding(
+  padding: EdgeInsets.only(top: 25.0, left: 20.0, right: 20.0),
+  child: Text(
+    'Bem-vindo',
+    textAlign: TextAlign.center,
+    overflow: TextOverflow.ellipsis,
+    style: TextStyle(fontWeight: FontWeight.bold),
+    textScaleFactor: 1.7,
+  )
+);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+Widget welcomeSubtext = const Padding(
+  padding: EdgeInsets.only(top: 25.0, left: 25.0, right: 25.0, bottom: 20.0),
+  child: Text(
+    'Bem-vindo ao ReservAPP, o melhor aplicativo para reserva de restaurantes!',
+    textAlign: TextAlign.center,
+    style: TextStyle(fontWeight: FontWeight.normal),
+    textScaleFactor: 1.3,
+  )
+);
+
+
+class RegisterButton extends StatefulWidget {
+  const RegisterButton({super.key});
+
+  @override
+  State<RegisterButton> createState() => _RegisterButton();
+}
+
+class _RegisterButton extends State<RegisterButton>{
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return Container(
+      alignment: Alignment.center,
+      padding: const EdgeInsets.all(20.0),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          minimumSize: const Size(300, 50),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10)
+          )
+        ),
+        onPressed: () => {
+          Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondRoute()),
+          )
+        },
+        child: const Text(
+          'Sou novo',
+          style: TextStyle(fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,
+          textScaleFactor: 1.3,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+}
+
+class LoginButton extends StatefulWidget {
+  const LoginButton({super.key});
+
+  @override
+  State<LoginButton> createState() => _LoginButton();
+}
+
+class _LoginButton extends State<LoginButton>{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      padding: const EdgeInsets.all(10.0),
+      child: GestureDetector(
+        onTap: () => {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SecondRoute()),
+          )
+        },
+        child: const Text(
+          'Já tenho conta',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontWeight: FontWeight.bold),
+          textScaleFactor: 1.3,
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
 }
